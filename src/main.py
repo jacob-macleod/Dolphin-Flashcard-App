7a9a5653adba4950ca95c0658bc62465234b9d45
jacob-macleod <jacob.e.macleod@gmail.com>
2024-01-16 17:23:28 +0000

    Added code to update goal status

--- src/main.py
+++ src/main.py
@@ -4,7 +4,7 @@ from routes.web.web_routes import web_routes
 from routes.api.authentication import authentication_routes
 from routes.api.statistics import statistics_routes
 from routes.api.card_management import card_management_routes
-from routes.api.goals import create_card_goal
+from routes.api.goals import update_goal_stats
 
 app = Flask(__name__, template_folder='../templates')
 
@@ -13,6 +13,7 @@ app.register_blueprint(authentication_routes)
 app.register_blueprint(statistics_routes)
 app.register_blueprint(card_management_routes)
 
-create_card_goal("uNQGiHzw1vODhLrm5CEkZCVhMOw1", "15", "20/01/2021")
+update_goal_stats("uNQGiHzw1vODhLrm5CEkZCVhMOw1", "10")
+
 if __name__ == "__main__":
     app.run(port=5000, host='0.0.0.0')
