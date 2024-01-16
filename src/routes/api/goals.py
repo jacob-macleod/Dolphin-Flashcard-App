bb7aa19b883bc21c7604831630a76227a9062d2b
jacob-macleod <jacob.e.macleod@gmail.com>
2024-01-16 17:23:28 +0000

    Added code to update goal status

--- src/routes/api/goals.py
+++ src/routes/api/goals.py
@@ -3,15 +3,55 @@ from classes.date import Date
 from database.database import database as db
 from routes.api.card_management import hash_to_numeric
 
-def update_goals(user_id) :
-    """ Update the user's goals, depending on whether it is
-    an XP or card goal """
+def update_goal_stats(user_id, xp_increment):
+    """Update the user's goal stats
+    To be run when a new card is revised"""
+
     # Get the user's goals
     goals = db.get("/users/" + user_id + "/goals/")
 
     # For each goal
     for goal in goals:
-        pass
+        # If the goal is in progress
+        if goals[goal].get("status") == "in progress":
+            if goals[goal].get("type") == "XP":
+                # Increment the user's XP
+                goals[goal]["data"]["starting_xp"] = str(
+                    int(goals[goal]["data"]["starting_xp"]) + int(xp_increment)
+                )
+            elif goals[goal].get("type") == "Card":
+                # Increment user's card goal
+                goals[goal]["data"]["cards_revised_so_far"] = str(
+                    int(goals[goal]["data"]["cards_revised_so_far"]) + 1
+                )
+
+    # Save the new goal data
+    db.save("/users/" + user_id + "/goals/", goals)
+
+def update_goal_status(user_id) :
+    """ Update the status of a goal (in process, completed or failed)"""
+    date = Date()
+    now = date.get_current_date()
+    goals = db.get("/users/" + user_id + "/goals/")
+
+    for goal in goals:
+        if goal[goal]["status"] == "in progress" :
+            # Check if the goal should be completed
+            if goal[goal]["type"] == "XP" :
+                if int(goal[goal]["data"]["starting_xp"]) >= int(goal[goal]["data"]["goal_xp"]) :
+                    goal[goal]["status"] = "completed"
+            elif goal[goal]["type"] == "Card" :
+                if int(goal[goal]["data"]["cards_revised_so_far"]) >= int(goal[goal]["data"]["cards_to_revise"]) :
+                    goal[goal]["status"] = "completed"
+            
+            # Check if goal should be failed - if now is after end date (and it is still in progress)
+            # Now and end date are both strings in dd/mm/yyyy format
+            if date.compare_dates(now, goal[goal]["end_date"]) > 0 :
+                goal[goal]["status"] = "failed"
+                goal[goal]["fail_date"] = now
+
+    # Save the new goal data
+    db.save("/users/" + user_id + "/goals/", goals)
 
 def create_xp_goal(user_id, goal_xp, end_date) :
     """ Create an XP goal for the user
