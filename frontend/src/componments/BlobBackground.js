0fd58ec9b7ad42e3522c4856133c2d44ecae8999
jacob-macleod <jacob.e.macleod@gmail.com>
2024-01-19 08:13:23 +0000

    Added initial background test

---
+++ frontend/src/componments/BlobBackground.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import './BlobBackground.css'
+import Blob from './Blob';
+
+
+function BlobBackground() {
+    return (
+        <div className="background">
+            <div className="backgroundPanel">
+                <Blob position={
+                    {
+                    float:"left",
+                    marginTop: "-100px"
+                    }
+                }
+                />
+
+                <Blob position={
+                    {
+                    float:"right",
+                    marginTop:"25vh",
+                    }
+                }
+                />
+
+                <Blob position={
+                    {
+                    float:"left",
+                    marginTop: "30vh",
+                    }
+                }
+                />
+            </div>
+        </div>
+    );
+}
+
+export default BlobBackground;
