b48d608a90e331eb7ef305aac94894cd463c7b16
jacob-macleod <jacob.e.macleod@gmail.com>
2024-01-19 08:13:23 +0000

    Added initial background test

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import BlobBackground from './componments/BlobBackground';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+      <div id="content" style={{position: "absolute", zIndex: "1", width: "maxWidth"}}>
+        <div style={{
+          display: "flex",
+          flexDirection: "column",
+          jutifyContent: "center",
+          height: "100vh",
+          margin: "32px",
+        }}>
+            <p class="header">We’re launching soon...</p>
+            <p class="subheader">Our goal? To design the best flashcard app ever by prioritizing users and their goals.</p>
+            <p class="text">We aim to launch in mid-2024. Want to learn more? Check out our <a href="https://github.com/jacob-macleod/Flashcard-App" class="link">Github Page!</a></p>
+            <p class="text"><span class="bold">Designer:</span> <a href="https://github.com/nathan-a-macleod" class="link">Nathan MacLeod</a></p>
+            <p class="text"><span class="bold">Developer:</span> <a href="https://github.com/jacob-macleod" class="link">Jacob MacLeod</a></p>
+        </div>            
     </div>
+      <BlobBackground />
+    </>
   );
 }
 
