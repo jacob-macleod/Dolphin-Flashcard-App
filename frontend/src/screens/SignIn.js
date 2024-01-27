dba42671473dff69ff9448937b776a5067d45a7d
jacob-macleod <jacob.e.macleod@gmail.com>
2024-01-27 12:21:30 +0000

    Made page responsive

--- frontend/src/screens/SignIn.js
+++ frontend/src/screens/SignIn.js
@@ -11,8 +11,8 @@ import Subheader from '../componments/Subheader';
 import Image from '../componments/Image';
 import Paragraph from '../componments/Paragraph';
 import Button from '../componments/Button';
-import WhiteOverlay from '../componments/WhiteOverlay';
 import DeveloperSection from '../componments/DeveloperSection';
+import BentoPanel from '../containers/BentoPanel';
 import goals from '../static/goals.png';
 import heatmap from '../static/heatmap.png';
 import card from '../static/card.png';
@@ -24,11 +24,44 @@ import '../componments/Bold.css';
 function SignInPage({ userID, setUserID, active=true }) {
   const [apikey, setApikey] = useState(null);
   const title = "Login";
+  const [width, setWidth] = useState(window.innerWidth);
+  const mobileBreakpoint = 878;
+  const tabletBreakpoint = 1200;
+  const [view, setView] = useState(width < mobileBreakpoint ? "mobile" : "desktop");
 
-  // Get the API key
+  const goalsDesktopSize = {width: "465px", height: "465px"};
+  const goalsTabletSize = {width: "310px", height: "310px"};
+  const heatmapDesktopSize = {width: "560px", height: "355px"};
+  const heatmapTabletSize = {width: "373px", height: "237px"};
+  const cardDesktopSize = {width: "558px", height: "373px"};
+  const cardTabletSize = {width: "372px", height: "249px"};
+
+
   useEffect(() => {
+    function handleResize() {
+      setWidth(window.innerWidth);
+    }
+  
+    // Get the API key
     getConfig(setApikey);
+  
+    // Set the initial window size
+    setWidth(window.innerWidth);
+  
+    // Set up the event listener for resize
+    window.addEventListener("resize", handleResize);
+
+    // Clean up the event listener when the component is unmounted
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
   }, []);
+
+  useEffect(() => {
+    setView(width < mobileBreakpoint ? "mobile" : "desktop");
+    console.log(width < mobileBreakpoint ? "mobile" : "desktop")
+    console.log(" width " + width)
+  }, [width]);
 
   const signInButton = active ? <Button text="Continue with Google" onClick={ () => {
       // Initialise the firebase project and sign in with google
@@ -49,7 +82,7 @@ function SignInPage({ userID, setUserID, active=true }) {
       <GridContainer layout="auto" classType="centered-grid-container">
         <div style={{width: "100%", height: "max-content"}}>
           <GridItem style={{
-            width: "70%",
+            width: "80%",
             margin: "auto",
           }}>
             <Image width="35px" height="40px"/>
@@ -57,54 +90,76 @@ function SignInPage({ userID, setUserID, active=true }) {
             <Paragraph text="Dolphin flashcards is a totally brand-new flashcard app - with one goal: to be better than anyone else on the market. Sign in for free to become part of it." />
             {signInButton}
 
-            <WhiteOverlay style={{display: "inline-flex"}}>
-              <div style={{width: "50%", padding: "16px", paddingLeft: "32px"}}>
+            <BentoPanel
+              item1 = {<div>
                 <Subheader text="Set goals, view them and see your progress - all in one place." />
                 <Paragraph text="Setting goals? Important.
                   Tracking them and achieving them? Even more. With Dolphin Flashcards
                   you can easily view and track your goals so you never have to explain
                   why you missed your deadline." />
-                {signInButton}
-              </div>
-                  <Image width='465px' height='465px' url={goals}/>
-            </WhiteOverlay>
+                {signInButton}</div>}
+              item2 = {<Image
+                width={width < tabletBreakpoint ? goalsTabletSize.width : goalsDesktopSize.width}
+                height={width < tabletBreakpoint ? goalsTabletSize.height : goalsDesktopSize.height}
+                url={goals}/>}
+              view={view}
+              style={{width: "50%", padding: "16px", paddingLeft: "32px"}}
+              />
 
-            <WhiteOverlay style={{display: "inline-flex"}}>
-              <Image width='559px' height='356px' url={heatmap}/>
-
-              <div style={{width: "50%", padding: "16px", paddingLeft: "32px"}}>
+            <BentoPanel
+            item1 = {<Image
+              width={width < tabletBreakpoint ? heatmapTabletSize.width : heatmapDesktopSize.width}
+              height={width < tabletBreakpoint ? heatmapTabletSize.height : heatmapDesktopSize.height}
+              url={heatmap}
+            />}
+            item2 = {
+              <div>
                 <Subheader text="Track & analyse your progress over time" />
                 <Paragraph text="With an integrated view
                 providing information on your streak and XP,
                 Dolphin Flashcards makes it easy to be consistent with studying." />
                 {signInButton}
               </div>
-            </WhiteOverlay>
+            }
+            view={view}
+            style={{width: "50%", padding: "16px", paddingLeft: "32px"}}
+            />
 
-            <WhiteOverlay style={{display: "inline-flex"}}>
-              <div style={{width: "50%", padding: "16px", paddingLeft: "32px"}}>
+            <BentoPanel
+              item1={<div>
                 <Subheader text="Memorise effectively with our spaced-repetition algorithm" />
                 <Paragraph text="Studying flashcards is vital.
                 But remembering them long-term? Even more.
                 That’s why we’ve built an effective spaced-repetition system to
                 help you remember what you study long into the future." />
                 {signInButton}
-              </div>
-                  <Image width='560px' height='355px' url={card}/>
-            </WhiteOverlay>
+              </div>}
+              item2={<Image
+                width={width < tabletBreakpoint ? cardTabletSize.width : cardDesktopSize.width}
+                height={width < tabletBreakpoint ? cardTabletSize.height : cardDesktopSize.height}
+                url={card}
+              />}
+              view={view}
+              style={{width: "50%", padding: "16px", paddingLeft: "32px"}}
+            />
 
-            <WhiteOverlay style={{display: "inline-flex", marginBottom:"450px"}}>
-              <Image width='558px' height='373px' url={cardEditor}/>
-
-              <div style={{width: "50%", padding: "16px", paddingLeft: "32px"}}>
+            <BentoPanel
+              overlayMarginBottom="550px"
+              item1={<Image
+                width={width < tabletBreakpoint ? cardTabletSize.width : cardDesktopSize.width}
+                height={width < tabletBreakpoint ? cardTabletSize.height : cardDesktopSize.height}
+                url={cardEditor}
+              />}
+              item2={<div>
                 <Subheader text="Add images, colors and more to your flashcards with our integrated editor" />
                 <Paragraph text="Our platform allows you to add images, colors, lists, bullet points
                 and more to your flashcards, so you can improve your recall by introducing visuals
                 to your flashcards." />
                 {signInButton}
-              </div>
-            </WhiteOverlay>
-
+              </div>}
+              view={view}
+              style={{width: "50%", padding: "16px", paddingLeft: "32px"}}
+            />
           </GridItem>
         <DeveloperSection />
       </div>
