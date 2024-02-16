9dedbc47e5b2e95e02a1947b5fe895a225ed9cfe
jacob-macleod <jacob.e.macleod@gmail.com>
2024-02-16 12:47:38 +0000

    Fixed goal date indicator

--- frontend/src/componments/Goal.js
+++ frontend/src/componments/Goal.js
@@ -6,25 +6,34 @@ import './Link.css';
 import './Goal.css';
 
 function Goal({ start, end, title, dueDate }) {
+    function formatDate(date, format) {
+        const map = {
+            mm: date.getMonth() + 1,
+            dd: date.getDate(),
+            yy: date.getFullYear().toString().slice(-2),
+            yyyy: date.getFullYear()
+        }
+    
+        return format.replace(/mm|dd|yy|yyy/gi, matched => map[matched])
+    }
+
     const calculateTimeLeft = () => {
         const dueDateParts = dueDate.split('/');
         const due = new Date(dueDateParts[2], dueDateParts[1] - 1, dueDateParts[0]); // Months are 0-indexed in JavaScript
         const now = new Date();
         const difference = due - now;
-
         if (difference <= 0) {
             const timeLeft = 'Expired';
             return timeLeft;
         }
-
         const hoursLeft = Math.floor((difference / (1000 * 60 * 60)) % 24);
         const daysLeft = Math.floor(difference / (1000 * 60 * 60 * 24));
         const monthsLeft = Math.floor(difference / (1000 * 60 * 60 * 24 * 30));
 
-        if (hoursLeft <= 24) {
+        if (daysLeft <= 0) {
             const timeLeft = `${hoursLeft} hour${hoursLeft !== 1 ? 's' : ''} left`;
             return timeLeft;
-        } else if (daysLeft <= 31) {
+        } else if (monthsLeft <= 0) {
             const timeLeft = `${daysLeft} day${daysLeft !== 1 ? 's' : ''} left`;
             return timeLeft;
         } else {
