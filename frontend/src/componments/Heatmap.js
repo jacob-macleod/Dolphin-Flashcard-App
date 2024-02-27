6d688f2d67bc9120778454adcd5f5a09301a4bde
jacob-macleod <jacob.e.macleod@gmail.com>
2024-02-27 17:06:55 +0000

    Finished HeatmapItem

--- frontend/src/componments/Heatmap.js
+++ frontend/src/componments/Heatmap.js
@@ -6,6 +6,12 @@ function Heatmap() {
     const days = {
     "01-02-2024": "2",
     "20-02-2024": "1",
+    "19-02-2024":"1",
+    "20-02-2024":"1",
+    "21-02-2024":"1",
+    "22-02-2024":"5",
+    "23-02-2024":"3",
+    "24-02-2024":"1",
     "25-02-2024":"3",
     "26-02-2024":"2"
     };
@@ -14,7 +20,7 @@ function Heatmap() {
 
     return (
         <div class="heatmap-container">
-            <HeatmapItem days={days} startDate={startDate} endDate={endDate}/>
+            <HeatmapItem days={days} startDate={startDate} endDate={endDate} month="January"/>
       </div>
     );
 }
--- frontend/src/componments/HeatmapItem.js
+++ frontend/src/componments/HeatmapItem.js
@@ -1,9 +1,31 @@
 import React from 'react';
 import './HeatmapItem.css';
+import Image from './Image';
+import Heading5 from './Heading5';
+import Paragraph from './Paragraph';
+import xIcon from '../static/blue-x-icon.svg';
 
-function HeatmapItem({days, startDate, endDate}) {
+function HeatmapItem({days, startDate, endDate, month}) {
     const [dayData, setDayData] = React.useState(null);
 
+    // Function to format a date as "dd-mm-yyyy"
+    function formatDate(date) {
+        let day = date.getDate();
+        let month = date.getMonth() + 1;
+        let year = date.getFullYear();
+
+        // Add leading zeros if necessary
+        if (day < 10) {
+            day = '0' + day;
+        }
+        if (month < 10) {
+            month = '0' + month;
+        }
+
+        return `${day}-${month}-${year}`;
+    }
+
+    React.useEffect(() => {
     // Generate the array of data points
     function generateDataArray() {
         // The data to store
@@ -33,26 +55,8 @@ function HeatmapItem({days, startDate, endDate}) {
         return data;
     }
 
-    // Function to format a date as "dd-mm-yyyy"
-    function formatDate(date) {
-        let day = date.getDate();
-        let month = date.getMonth() + 1;
-        let year = date.getFullYear();
-
-        // Add leading zeros if necessary
-        if (day < 10) {
-            day = '0' + day;
-        }
-        if (month < 10) {
-            month = '0' + month;
-        }
-
-        return `${day}-${month}-${year}`;
-    }
-
-    React.useEffect(() => {
         setDayData(generateDataArray());
-    }, []);
+    }, [days, endDate, startDate]);
 
     React.useEffect(() => {
         console.log(dayData);
@@ -60,11 +64,32 @@ function HeatmapItem({days, startDate, endDate}) {
 
     // Helper function to get the starting day of the month
     function getStartingDayOfMonth(dateString) {
-        const [day, month, year] = dateString.split("-");
+        const month = dateString.split("-")[1];
+        const year = dateString.split("-")[2];
         const date = new Date(`${year}-${month}-01`);
         return date.getDay(); // Returns 0 for Sunday, 1 for Monday, etc.
     }
-      
+
+    function getHighestValue() {
+        if (!dayData) {
+            return null;
+        }
+        let highestValue = 0;
+    
+        // Iterate through each value in dayData
+        for (const value of Object.values(dayData)) {
+            // Parse the value as a number and compare with highestValue
+            const numericValue = parseInt(value);
+            if (!isNaN(numericValue) && numericValue > highestValue) {
+                highestValue = numericValue;
+            }
+        }
+    
+        return highestValue;
+    }
+
+    const noStreakIcon = <div className='image-wrapper'><Image url={xIcon} width='12px' height='12px' paddingRight='0px'/></div>;
+    const highestStreakValue = getHighestValue();
     function renderTableHeaders(dayData) {
         // Check if dayData is null
         if (!dayData) {
@@ -81,9 +106,9 @@ function HeatmapItem({days, startDate, endDate}) {
         var addedStartPadding = false;
         for (const item in dayData) {
             // Add padding to the start
-            if (addedStartPadding == false) {
+            if (addedStartPadding === false) {
                 for (var i=0; i<startingDayOfMonth; i++) {
-                    tableRows.push(<th key={count}></th>);
+                    tableRows.push(<th key={count} className='table-item'></th>);
                     count ++;
                 }
                 addedStartPadding = true;
@@ -91,15 +116,27 @@ function HeatmapItem({days, startDate, endDate}) {
             // Extract the day and data value from the item
             const day = item.charAt(0); // Extract the first character (day)
             const dataValue = dayData[item]; // Get the data value
-    
-            // Append a <th> element with the data value to the tableHeaders array
-            tableRows.push(<th key={day}>{dataValue}</th>);
+            
+            const relativeStreakSize = parseInt(dataValue) / highestStreakValue;
+            // Append the correct item to the array
+            if (dataValue === "0") {
+                tableRows.push(<th key={day} className='table-item'>{noStreakIcon}</th>);
+            } else {
+                tableRows.push(<th key={day} className='table-item'>
+                    <div className={
+                        relativeStreakSize < 0.2 ? "low-streak" :
+                        relativeStreakSize < 0.5 ? "medium-streak" :
+                        relativeStreakSize < 0.7 ? "high-streak" :
+                        "highest-streak"
+                    }></div>
+                </th>);
+            }
             count ++;
 
             // If 7 items have been added or it's the last item
             if (count === 7 || Object.keys(dayData).indexOf(item) === Object.keys(dayData).length - 1) {
                 // Add the table row to tableHeaders
-                tableHeaders.push(<tr key={count}>{tableRows}</tr>);
+                tableHeaders.push(<tr key={count} className='table-row'>{tableRows}</tr>);
                 tableRows = [];
                 count = 0;
             }
@@ -108,18 +145,24 @@ function HeatmapItem({days, startDate, endDate}) {
     }
 
     return (
-        <table>
-            <tr>
-                <th>M</th>
-                <th>T</th>
-                <th>W</th>
-                <th>T</th>
-                <th>F</th>
-                <th>S</th>
-                <th>S</th>
-            </tr>
-            {renderTableHeaders(dayData)}
-        </table>
+        <div class="heatmap-item-container">
+            <Paragraph text={month} style={{
+                marginTop: "4px",
+                marginBottom: "4px",
+            }}/>
+            <table>
+                <tr className='table-row'>
+                    <th className='table-item'><Heading5 color='black' text="M"/></th>
+                    <th className='table-item'><Heading5 color='black' text="T"/></th>
+                    <th className='table-item'><Heading5 color='black' text="W"/></th>
+                    <th className='table-item'><Heading5 color='black' text="T"/></th>
+                    <th className='table-item'><Heading5 color='black' text="F"/></th>
+                    <th className='table-item'><Heading5 color='black' text="S"/></th>
+                    <th className='table-item'><Heading5 color='black' text="S"/></th>
+                </tr>
+                {renderTableHeaders(dayData)}
+            </table>
+        </div>
     );
 }
 
