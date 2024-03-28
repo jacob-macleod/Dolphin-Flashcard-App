afa88e461da018e6a2bcb16c718483a34b086896
jacob-macleod <jacob.e.macleod@gmail.com>
2024-03-28 20:47:00 +0000

    Updated workflow

--- bump_version.sh
+++ bump_version.sh
@@ -9,7 +9,7 @@ fi
 
 # Read current version from version.txt
 entered_version=$(grep -Eo "__version__ = \"[0-9]+\.[0-9]+\.[0-9]+\"" "$init_file" | awk -F '"' '{print $2}')
-current_version=$(git describe --tags --abbrev=0)
+current_version=$(git describe --tags --abbrev=0 --always)
 
 echo "Checking version $current_version has been updated correctly according to pull request name '$1'"
 
