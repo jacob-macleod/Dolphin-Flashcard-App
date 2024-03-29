58a7b98104b140c4bedb5bff6821a93cd99f2e8d
Jacob MacLeod <48350114+jacob-macleod@users.noreply.github.com>
2024-03-29 22:33:24 +0000

    Update config.js

--- frontend/src/api/config.js
+++ frontend/src/api/config.js
@@ -1,3 +1,2 @@
-//const serverURL = "http://dolphinflashcards.com/api/";
-const serverURL = "http://127.0.0.1:5000/api/";
+const serverURL = "http://dolphinflashcards.com/api/";
 export default serverURL;
