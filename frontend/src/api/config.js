5aa71024f8fa3e504a4a5ee476c147946d0a1f9d
jacob-macleod <jacob.e.macleod@gmail.com>
2024-04-04 18:29:50 +0100

    Updated commit files

--- frontend/src/api/config.js
+++ frontend/src/api/config.js
@@ -1,3 +1,3 @@
-//const serverURL = "http://dolphinflashcards.com/api/";
-const serverURL = "http://localhost:5000/api/";
+const serverURL = "http://dolphinflashcards.com/api/";
+//const serverURL = "http://localhost:5000/api/";
 export default serverURL;
