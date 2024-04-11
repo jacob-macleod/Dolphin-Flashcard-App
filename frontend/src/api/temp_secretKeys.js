7dfb7bdbebc87abe044ab02fcc99e24ea1c6d27d
jacob-macleod <jacob.e.macleod@gmail.com>
2024-04-11 18:46:27 +0100

    Edited workflow

---
+++ frontend/src/api/temp_secretKeys.js
@@ -0,0 +1,1 @@
+const mailChimpApiKey = "https://dolphinflashcards.us18.list-manage.com/subscribe/post?u=912e840a9d7addf77060a3290&amp;id=2636de219f&amp;f_id=009fc2e1f0";export default mailChimpApiKey;
