9b232e690b80fd3c36f5a755a03b536956acac5f
jacob-macleod <jacob.e.macleod@gmail.com>
2024-04-15 18:39:19 +0100

    Began rafactoring NewGoalPopup

--- frontend/src/containers/Modal/index.js
+++ frontend/src/containers/Modal/index.js
@@ -1,3 +1,4 @@
 export { default as CardOperationsPopup } from './CardOperationsPopup';
 export { default as EditGoalPopup } from './EditGoalPopup';
 export {default as MoveFolderDialogue } from './MoveFolderDialogue';
+import {default as Modal} from './Modal';
