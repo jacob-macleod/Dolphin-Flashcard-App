7cfb7826f83d0345b7eb68e672204fa3542af976
jacob-macleod <jacob.e.macleod@gmail.com>
2024-04-26 21:29:55 +0100

    Tweaked workflow

--- testing/run_tests.sh
+++ testing/run_tests.sh
@@ -1,4 +1,17 @@
+#!/bin/bash
+
+# Start Firestore emulator
+bash firebase_emulator.sh &
+
+# Wait for the emulator to start
+echo "Waiting 10 seconds for emulator to start..."
+sleep 10
+
+# Run unit tests
 python3 -m venv env
 source env/bin/activate
 pip3 install -r requirements.txt
 pytest testing/
+
+# Stop Firestore emulator
+docker stop $(docker ps -aq --filter "ancestor=mtlynch/firestore-emulator-docker")
