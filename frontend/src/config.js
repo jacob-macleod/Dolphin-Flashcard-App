9a9c02032087f8807791a5b6f2608b7222869a5d
jacob-macleod <jacob.e.macleod@gmail.com>
2025-08-21 20:17:21 +0100

    Added config file

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -2,7 +2,7 @@
 // FYI: Importing flashcards from anki (or others) takes alot of processing power & time, usually exceeds allowed time limit in the browser. Disabled by default until more powerful server available
 
 export const ankiImportsDisabled = true;
-export const quizletImportsDisabled = true;
+// export const quizletImportsDisabled = true;
 
 // export const ankiImportsDisabled = false;
-// export const quizletImportsDisabled = false;
+export const quizletImportsDisabled = false;
